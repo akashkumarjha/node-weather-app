16a170af9bd896b58fbecbae5de292bfd5ebab42
akashkumarjha <akashjha513@gmail.com>
2020-10-13 18:24:57 +0530

    style change

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,22 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('.message-1')
 const messageTwo = document.querySelector('.message-2')
+document.getElementById("myP").style.display="none";
 
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const address=search.value;
-    messageOne.textContent = 'Loading';
-    messageTwo.textContent='';
+    messageOne.textContent = '';
+    document.getElementById("myP").style.display="none";
+    messageTwo.textContent='Loading';
     fetch('/weather?address='+address).then((response) => {
     response.json().then((data) => {
        if(data.message){
            //console.log(data.message)
            messageOne.textContent=data.message;
        } else{
-           messageOne.textContent=data.forecast+" Degree"
+        document.getElementById("myP").style.display="block";
+           messageOne.textContent=data.forecast
            messageTwo.textContent=data.location
           // console.log(data.forecast)
           // console.log(data.location)
